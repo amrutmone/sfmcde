2bb65639fa6840475f8bb8724adcc55c84d7ff67
amrutmone <137758580+amrutmone@users.noreply.github.com>
2023-07-24 01:06:27 +0530

    Update sfmc-client.js

--- utils/sfmc-client.js
+++ utils/sfmc-client.js
@@ -4,9 +4,9 @@ const options = {
   auth: {
     clientId: process.env.SFMC_CLIENT_ID,
     clientSecret: process.env.SFMC_CLIENT_SECRET,
+    accountId: process.env.SFMC_ACCOUNT_ID,
     authOptions: {
       authVersion: 2,
-      accountId: process.env.SFMC_ACCOUNT_ID,
     },
     authUrl: `https://${process.env.SFMC_SUBDOMAIN}.auth.marketingcloudapis.com/v2/token`,
   },
